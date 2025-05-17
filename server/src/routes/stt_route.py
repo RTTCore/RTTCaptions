32e2a3ba77733b21bf5b59e7d01f7f9027c91fa4
akramcodez <skcodewizard786@gmail.com>
2025-05-17 19:08:48 +0530

    Setup_GPU_transcription/akram c.4

--- server/src/routes/stt_route.py
+++ server/src/routes/stt_route.py
@@ -1,13 +1,24 @@
 from fastapi import APIRouter, UploadFile, File
 from src.stt.whisper_wrapper import STTModel  
 import shutil
+import time 
 
 router = APIRouter()
 model = STTModel()
 
 @router.post("/transcribe")
 async def transcribe_audio(file: UploadFile = File(...)):
+    start_time = time.time() 
+
     with open("temp.wav", "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
+
     result = model.transcribe("temp.wav")
-    return {"transcription": result}
+
+    end_time = time.time() 
+    response_time = round(end_time - start_time, 3)
+
+    return {
+        "transcription": result,
+        "response_time": f"{response_time} seconds"
+    }
